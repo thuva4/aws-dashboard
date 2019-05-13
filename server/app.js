c764b21efe8d1ca986dddcb2fac021282805b180
Tharmarajasingam Thuvarakan <tharma.thuva@gmail.com>
2019-05-13 20:58:15 +0530

    fix
    Signed-off-by: Tharmarajasingam Thuvarakan <tharma.thuva@gmail.com>

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'html');
+// app.set('view engine', 'html');
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
@@ -25,14 +25,14 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
