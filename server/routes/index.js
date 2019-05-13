b85ab6d31723c83e9e4f95d4423c2bdd778b7da6
Tharmarajasingam Thuvarakan <tharma.thuva@gmail.com>
2019-05-13 22:53:10 +0530

    fix
    Signed-off-by: Tharmarajasingam Thuvarakan <tharma.thuva@gmail.com>

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,12 @@ AWS.config.update({'region': 'us-east-1'})
 const axios = require('axios');
 const fs = require('fs');
 
-router.get('/createtempCredentials', function(req, res, next) {
-        let roleArn = `arn:aws:iam::315465781430:role/Application-CDE-MSD-DevOps-Role`;
+router.get('/createtempCredentials', async function(req, res, next) {
+        let roleArn =await axios.get("http://169.254.169.254/latest/meta-data/iam/info")	    
+            .then(response => response.data)	     
+            .then(data => {	      
+             return data.InstanceProfileArn.replace("instance-profile", "role");;
+        })
         console.log("Assuming role: "+roleArn);
         let sts = new AWS.STS() ;
         sts.assumeRole({RoleArn: roleArn, RoleSessionName: 'SnapshotGraphs'}, function(err, data) {
@@ -29,23 +33,23 @@ router.get('/gettempCredentials', function(req, res, next){
         if (err) res.status(500).send(err)
         else {
             data = JSON.parse(data)
-            // res.send(data.Credentials)
-            let tempCredentials = new AWS.Credentials(data.Credentials.AccessKeyId, 
-                data.Credentials.SecretAccessKey, 
-                data.Credentials.SessionToken)
-            let cloudWatch = tempCredentials ? new AWS.CloudWatch({credentials:tempCredentials}) : new AWS.CloudWatch();
-            cloudWatch.listDashboards({}, function(err, data) {
-                if (err) {
-                    return res.status(500).send(err);
-                } else {
-                  console.log(data.DashboardEntries)
-                  let dataJson = {
-                    DashboardEntries:data.DashboardEntries
-                  }
-                  console.log(dataJson)
-                  res.send(dataJson)
-                }
-              } );
+            res.send(data.Credentials)
+            // let tempCredentials = new AWS.Credentials(data.Credentials.AccessKeyId, 
+            //     data.Credentials.SecretAccessKey, 
+            //     data.Credentials.SessionToken)
+            // let cloudWatch = tempCredentials ? new AWS.CloudWatch({credentials:tempCredentials}) : new AWS.CloudWatch();
+            // cloudWatch.listDashboards({}, function(err, data) {
+            //     if (err) {
+            //         return res.status(500).send(err);
+            //     } else {
+            //       console.log(data.DashboardEntries)
+            //       let dataJson = {
+            //         DashboardEntries:data.DashboardEntries
+            //       }
+            //       console.log(dataJson)
+            //       res.send(dataJson)
+            //     }
+            //   } );
         }
     })
 })
